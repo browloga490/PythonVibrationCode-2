832a964f79ad4020f80a40351102def2b44bed91
Logan Brown <lbrow077@uottawa.ca>
2018-08-17 16:58:07 -0400

    Created the 'graph_class.py' script that acts as a library that can be imported by another Python script - it defines a class for an object of type <Graph> and and object of type <Graph_Frame>. Updated 'MachineMonitorFinal.py' - commented out all previous imports, created an import for the 'graph_class' library, commented out all of the code that ran as the main program of this file (see '##START OF PROGRAM##' on line 529), created objects of type <Graph> and stored them in an object of type <Graph_Frame>, called the <Graph_Frame> objet's built in 'build_graphs' function to construct each graph and save them to the designated GUI graph folder for Channel 0. Removed out-of-date graph images from the working directory. Updated 'master_mem.txt' - removed all of the data entries that were previously stored in it, added new data entries that were compatible with the newly updated 'MachineMonitorFinal.py' file.

--- MachineMonitorFinal.py
+++ MachineMonitorFinal.py
@@ -1,22 +1,25 @@
-import matplotlib as mpl
-mpl.use('TkAgg')
-
-import scipy.fftpack
-import matplotlib.dates as dates
-import ast
-import datetime
+##import matplotlib as mpl
+##mpl.use('TkAgg')
+##
+##import scipy.fftpack
+##import matplotlib.dates as dates
+##import ast
+##import datetime
 import os 
-import csv
-import shutil
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
-from matplotlib.colors import Colormap
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from mpl_toolkits.mplot3d import axes3d
-from matplotlib.pyplot import figure
+##import csv
+##import shutil
+##import numpy as np
+##import matplotlib.pyplot as plt
+##from matplotlib.colors import LinearSegmentedColormap
+##from matplotlib.colors import Colormap
+##from mpl_toolkits.axes_grid1 import make_axes_locatable
+##from mpl_toolkits.mplot3d import axes3d
+##from matplotlib.pyplot import figure
+#import graph_class
+from graph_class import Graph_Frame
+from graph_class import Graph
 
-os.chdir(r"C:\Users\Owner\Documents\RVMD Job Stuff\PythonVibrationCode 2")
+os.chdir(r"/usr/local/RVMD/Python_Scripts/Graph_Gen/GenCh0/")
 
 # CONSTANTS
 FILE_PREFIX = "data_store/"
@@ -168,7 +171,7 @@ def get_data(filenames):
     return datetrack
 
 
-def build_graph(scope, m_class):
+def build_average_graph(scope, m_class):
     
     now = datetime.date.today()
     
@@ -191,7 +194,7 @@ def build_graph(scope, m_class):
 
     dayrmsn.append(temp[0])
     daypkpkn.append(temp[1])
-    dates.append(temp[2].strip())
+    dates.append(temp[2].strip()) #will append temp[7] on CentOS machines
 
     for row in csv_reader:
 
@@ -334,7 +337,7 @@ def build_graph(scope, m_class):
     plt.savefig('C1_PKPK_'+str(scope)+'.png',facecolor='k',dpi=200)#'#151515')
     #plt.show()
 
-def build_bearing_graph(scope, m_class):    #Find a way to incorporate the scope into the name of the graph image
+def build_2_graph(scope, m_class):    #Find a way to incorporate the scope into the name of the graph image
     
     now = datetime.date.today()
     
@@ -346,33 +349,27 @@ def build_bearing_graph(scope, m_class):    #Find a way to incorporate the scope
     next(csv_reader)
     temp = next(csv_reader)
     
-    dayrmsn = []    
-    daypkpkn = []
+    some_value_2 = []    
     dates = []
-    plotr = []
-    plotp = []
+    plot = []
 
     fig = plt.figure(facecolor='#151515',figsize=(10,5))
     ax1 = fig.add_subplot(111)
 
-    dayrmsn.append(temp[0])
-    daypkpkn.append(temp[1])
-    dates.append(temp[2].strip())
+    some_value_2.append(temp[2])
+    dates.append(temp[7].strip())
 
     for row in csv_reader:
 
         if row[2].strip() == "END":
-            r = np.mean([float(i) for i in dayrmsn])
-            p = np.mean([float(i) for i in daypkpkn])
+            r = np.mean([float(i) for i in some_value_2])
 
-            plotr.append(r)
-            plotp.append(p)
+            plot.append(r)
             
             final = datetime.datetime.strptime(dates[-1], '%m-%d-%Y').date()
             
             for i in range(0, scope - len(dates)):
-                plotr.append(0.0)
-                plotp.append(0.0)
+                plot.append(0.0)
                 
                 date = final - datetime.timedelta(days=i) #Change final to first value found (maybe dates[0]?)
 
@@ -386,29 +383,23 @@ def build_bearing_graph(scope, m_class):    #Find a way to incorporate the scope
             
                 if date == datetime.datetime.strptime(dates[-1], '%m-%d-%Y').date():
                     
-                    dayrmsn.append(row[0])  #Adding contents of row[0] to a list of Strings
-                    daypkpkn.append(row[1])
+                    some_value_2.append(row[2])  #Adding contents of row[2] to a list of Strings
                     
                 else:
-                    r = np.mean([float(i) for i in dayrmsn]) 
-                    p = np.mean([float(i) for i in daypkpkn])
+                    r = np.mean([float(i) for i in some_value_2]) 
 
-                    plotr.append(r)
-                    plotp.append(p)
+                    plot.append(r)
 
-                    dayrmsn = [row[0]]    
-                    daypkpkn = [row[1]]
+                    some_value_2 = [row[2]]    
                     
                     dates.append('{:02d}'.format(date.month) + '-' + '{:02d}'.format(date.day) + '-' + str(date.year))
 
             else:
-                r = np.mean([float(i) for i in dayrmsn]) 
-                p = np.mean([float(i) for i in daypkpkn])
+                r = np.mean([float(i) for i in some_value_2])
 
-                plotr.append(r)
-                plotp.append(p)
+                plot.append(r)
 
-                dayrmsn = [row[0]]    
+                some_value_2 = [row[2]]    
                 daypkpkn = [row[1]]
                 
                 break
@@ -467,70 +458,70 @@ def build_bearing_graph(scope, m_class):    #Find a way to incorporate the scope
     plt.savefig('C1_BEAR_'+str(scope)+'.jpg',facecolor='k',dpi=200)#'#151515')
     #plt.show()
 
-def build_fft_graph(filenames,N,T):
-
-    sec = []
-    gs = []
-    file_list = []
-    
-    file = open('storage.txt', 'r+')
-    csv_reader = csv.reader(file)
-    next(csv_reader)
-    file.close()
-
-    if len(filenames) >= 10:
-        file_list = filenames[-10:]
-
-    else:
-        file_list = filenames
-
-    ##START FOR LOOP##
-
-    file = open(FILE_PREFIX + 'measure80.txt')#'50HzTXT.txt')
-    file.readline()
-    file.readline()
-
-    rows = file.readlines()
-
-    for row in rows:
-        cols = row.split(",")
-        count = 0
-
-        for col in cols:
-            if count == 0:
-                sec.append(float(col)) #(word.strip()) is an alternative
-            elif count == 1:
-                gs.append(float(col))
-                break
-            count += 1
-
-    
-
-    XYZ_list = [] #Consider storing a copy of this list in the storage.txt folder (oh and you've got this :-)
-    
-    x = sec
-    y = gs
-    yf = scipy.fftpack.fft(y)
-    xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
-
-    temp = [4]*24
-
-    yn = np.array(temp) 
-    zn = 2.0/N * np.abs(yf[:N//2])
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    
-    ax.plot(xf,yn,zn)
-
-    ##END FOR LOOP##
-
-    ax.set_xlabel('x axis')
-    ax.set_ylabel('y axis')
-    ax.set_zlabel('z axis')
-
-    plt.tight_layout()
-    plt.show(fig)
+##def build_fft_graph(filenames,N,T):
+##
+##    sec = []
+##    gs = []
+##    file_list = []
+##    
+##    file = open('storage.txt', 'r+')
+##    csv_reader = csv.reader(file)
+##    next(csv_reader)
+##    file.close()
+##
+##    if len(filenames) >= 10:
+##        file_list = filenames[-10:]
+##
+##    else:
+##        file_list = filenames
+##
+##    ##START FOR LOOP##
+##
+##    file = open(FILE_PREFIX + 'measure80.txt')#'50HzTXT.txt')
+##    file.readline()
+##    file.readline()
+##
+##    rows = file.readlines()
+##
+##    for row in rows:
+##        cols = row.split(",")
+##        count = 0
+##
+##        for col in cols:
+##            if count == 0:
+##                sec.append(float(col)) #(word.strip()) is an alternative
+##            elif count == 1:
+##                gs.append(float(col))
+##                break
+##            count += 1
+##
+##    
+##
+##    XYZ_list = [] #Consider storing a copy of this list in the storage.txt folder (oh and you've got this :-)
+##    
+##    x = sec
+##    y = gs
+##    yf = scipy.fftpack.fft(y)
+##    xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+##
+##    temp = [4]*24
+##
+##    yn = np.array(temp) 
+##    zn = 2.0/N * np.abs(yf[:N//2])
+##
+##    fig = plt.figure()
+##    ax = fig.add_subplot(111, projection='3d')
+##    
+##    ax.plot(xf,yn,zn)
+##
+##    ##END FOR LOOP##
+##
+##    ax.set_xlabel('x axis')
+##    ax.set_ylabel('y axis')
+##    ax.set_zlabel('z axis')
+##
+##    plt.tight_layout()
+##    plt.show(fig)
 
 
 
@@ -541,20 +532,115 @@ def build_fft_graph(filenames,N,T):
 
 #filedates = list(set(get_data(filenames)))
 
-g_cmap = [colormap_builder(1,good[0],0),colormap_builder(1,good[1],1)]
-s_cmap = [colormap_builder(1,satisfactory[0],2),colormap_builder(1,satisfactory[1],3)]
-uns_cmap = [colormap_builder(1,unsatisfactory[0],4),colormap_builder(1,unsatisfactory[1],5)]
-una_cmap = [colormap_builder(1,unacceptable[0],6),colormap_builder(1,unacceptable[1],7)]
-
-build_graph(7, 1)
-build_graph(31, 1)
-build_graph(93, 1)
-build_graph(186, 1)
-build_bearing_graph(7, 1)
+##g_cmap = [colormap_builder(1,good[0],0),colormap_builder(1,good[1],1)]
+##s_cmap = [colormap_builder(1,satisfactory[0],2),colormap_builder(1,satisfactory[1],3)]
+##uns_cmap = [colormap_builder(1,unsatisfactory[0],4),colormap_builder(1,unsatisfactory[1],5)]
+##una_cmap = [colormap_builder(1,unacceptable[0],6),colormap_builder(1,unacceptable[1],7)]
+##
+##build_average_graph(7, 1)
+##build_average_graph(31, 1)
+##build_average_graph(93, 1)
+##build_average_graph(186, 1)
+##build_bearing_graph(7, 1)
 
 
 #build_fft_graph(filenames,48,1/500)
 #print(cbar)
+
+frame = Graph_Frame(r'/usr/local/RVMD/GUI/Ch0_Graphs/')
+
+#graph_X = Graph(ID, *kwargs)
+
+graph_1 = Graph('Ch0_AVG',has_cbar=True,has_gradient=True)
+graph_2 = Graph('Ch0_AVG',has_cbar=True,has_gradient=True)
+graph_3 = Graph('Ch0_AVG',has_cbar=True,has_gradient=True)
+graph_4 = Graph('Ch0_AVG',has_cbar=True,has_gradient=True)
+
+graph_5 = Graph('Ch0_LOOSE',fill_color='lightsteelblue')
+graph_6 = Graph('Ch0_LOOSE',fill_color='lightsteelblue')
+graph_7 = Graph('Ch0_LOOSE',fill_color='lightsteelblue')
+graph_8 = Graph('Ch0_LOOSE',fill_color='lightsteelblue')
+
+graph_9 = Graph('Ch0_MISA',fill_color='honeydew')
+graph_10 = Graph('Ch0_MISA',fill_color='honeydew')
+graph_11 = Graph('Ch0_MISA',fill_color='honeydew')
+graph_12 = Graph('Ch0_MISA',fill_color='honeydew')
+
+graph_13 = Graph('Ch0_BEAR',fill_color='limegreen')
+graph_14 = Graph('Ch0_BEAR',fill_color='limegreen')
+graph_15 = Graph('Ch0_BEAR',fill_color='limegreen')
+graph_16 = Graph('Ch0_BEAR',fill_color='limegreen')
+
+graph_17 = Graph('Ch0_GEARS',fill_color='plum')
+graph_18 = Graph('Ch0_GEARS',fill_color='plum')
+graph_19 = Graph('Ch0_GEARS',fill_color='plum')
+graph_20 = Graph('Ch0_GEARS',fill_color='plum')
+
+graph_21 = Graph('Ch0_OTHER',fill_color='thistle')
+graph_22 = Graph('Ch0_OTHER',fill_color='thistle')
+graph_23 = Graph('Ch0_OTHER',fill_color='thistle')
+graph_24 = Graph('Ch0_OTHER',fill_color='thistle')
+
+graph_1.read_file('master_mem.txt',1,7,7)
+graph_2.read_file('master_mem.txt',1,7,31)
+graph_3.read_file('master_mem.txt',1,7,93)
+graph_4.read_file('master_mem.txt',1,7,186)
+
+graph_5.read_file('master_mem.txt',2,7,7)
+graph_6.read_file('master_mem.txt',2,7,31)
+graph_7.read_file('master_mem.txt',2,7,93)
+graph_8.read_file('master_mem.txt',2,7,186)
+
+graph_9.read_file('master_mem.txt',3,7,7)
+graph_10.read_file('master_mem.txt',3,7,31)
+graph_11.read_file('master_mem.txt',3,7,93)
+graph_12.read_file('master_mem.txt',3,7,186)
+
+graph_13.read_file('master_mem.txt',4,7,7)
+graph_14.read_file('master_mem.txt',4,7,31)
+graph_15.read_file('master_mem.txt',4,7,93)
+graph_16.read_file('master_mem.txt',4,7,186)
+
+graph_17.read_file('master_mem.txt',5,7,7)
+graph_18.read_file('master_mem.txt',5,7,31)
+graph_19.read_file('master_mem.txt',5,7,93)
+graph_20.read_file('master_mem.txt',5,7,186)
+
+graph_21.read_file('master_mem.txt',6,7,7)
+graph_22.read_file('master_mem.txt',6,7,31)
+graph_23.read_file('master_mem.txt',6,7,93)
+graph_24.read_file('master_mem.txt',6,7,186)
+
+#print(graph_1.x_axis)
+#print(graph_1.y_axis)
+
+frame.add_graph(graph_1)
+frame.add_graph(graph_2)
+frame.add_graph(graph_3)
+frame.add_graph(graph_4)
+frame.add_graph(graph_5)
+frame.add_graph(graph_6)
+frame.add_graph(graph_7)
+frame.add_graph(graph_8)
+frame.add_graph(graph_9)
+frame.add_graph(graph_10)
+frame.add_graph(graph_11)
+frame.add_graph(graph_12)
+frame.add_graph(graph_13)
+frame.add_graph(graph_14)
+frame.add_graph(graph_15)
+frame.add_graph(graph_16)
+frame.add_graph(graph_17)
+frame.add_graph(graph_18)
+frame.add_graph(graph_19)
+frame.add_graph(graph_20)
+frame.add_graph(graph_21)
+frame.add_graph(graph_22)
+frame.add_graph(graph_23)
+frame.add_graph(graph_24)
+
+frame.build_colormaps()
+frame.build_graphs()
 
 #END OF PROGRAM#
 
